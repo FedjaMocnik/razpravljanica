701f5d13db01b088c9c95ac72cdd9be24040892c
Fedja Mocnik <fedja.mocnik@gmail.com>
2025-12-07 22:14:15 +0100

    proto + gen_proto

---
+++ gen_protos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+protoc \
+    --go_out=pb --go_opt=paths=source_relative \
+    --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+    -I proto \
+    proto/razpravljalnica.proto
