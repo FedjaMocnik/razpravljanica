4dd48583ec226571f1db77074dd217552756a109
Fedja Mocnik <fedja.mocnik@gmail.com>
2025-12-16 21:16:30 +0100

    Private API proto + sh change

--- gen_protos.sh
+++ gen_protos.sh
@@ -5,3 +5,9 @@ protoc \
     --go-grpc_out=pkgs/public/pb --go-grpc_opt=paths=source_relative \
     -I pkgs/public/proto \
     pkgs/public/proto/razpravljalnica.proto
+
+protoc \
+    --go_out=pkgs/private/pb --go_opt=paths=source_relative \
+    --go-grpc_out=pkgs/private/pb --go-grpc_opt=paths=source_relative \
+    -I pkgs/private/proto \
+    pkgs/private/proto/replikacija.proto
